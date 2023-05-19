54765fcd827fb31d2b9722235d075cea3d8e3979
dovicie <hyokoyama8810@gmail.com>
2023-05-20 08:36:17 +0900

    🎨 Prettier

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,17 @@
-import '@/styles/globals.css'
-import { MantineProvider } from '@mantine/core';
-import type { AppProps } from 'next/app'
-import Head from 'next/head';
+import "@/styles/globals.css";
+import { MantineProvider } from "@mantine/core";
+import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
         <title>Page title</title>
-        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
       </Head>
 
       <MantineProvider
@@ -16,10 +19,11 @@ export default function App({ Component, pageProps }: AppProps) {
         withNormalizeCSS
         theme={{
           /** Put your mantine theme override here */
-          colorScheme: 'light',
+          colorScheme: "light",
         }}
       >
         <Component {...pageProps} />
       </MantineProvider>
     </>
-  );}
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
-import { Inter } from "next/font/google";
-import { Button, Select } from "@mantine/core";
 import { useMediaQuery } from "@/lib/mantine/useMediaQuery";
+import { Button, Select } from "@mantine/core";
+import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
